6351d7c7e216d73d24cfde298732e5a582060dac
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-10-01 20:41:56 +0900

    投稿確認画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :feeds
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :feeds do
+    collection do
+     post :confirm
+    end
+  end
 end
