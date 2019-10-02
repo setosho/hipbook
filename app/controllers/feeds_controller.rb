8b2afd196b97bd65cff31e95331615aeae9f77c8
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-10-02 12:34:41 +0900

    投稿画面のエラーを修正

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -75,7 +75,7 @@ class FeedsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_feed
-      @feed = Feed.find(params[:id])
+      @feed = Feed.new
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
